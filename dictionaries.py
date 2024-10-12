21a4382da38bae7f69145d9d90e85c2b05d6aeca
domme7 <dominik.hu@outlook.de>
2024-10-12 16:43:14 +0200

    hinzufügen von Print und hinzufügen von hobby

--- dictionaries.py
+++ dictionaries.py
@@ -3,3 +3,13 @@ person = {
     "age": 30,
     "beruf": "Informatiker"
 }
+
+#Zugriff auf Werte über die Schlüssel
+print(f"Name: {person['name']}")
+print(f"Age: {person['age']}")
+print(f"Beruf: {person['beruf']}")
+
+# Hinzufügen eines neuen Schlüssel
+
+person["hobby"] = "Programmieren"
+print(f"Hobby: {person['hobby']}")
